883c6977369b56f551b077cbe16596c788ec200b
Danik Gorshkov <rock_dmj@mail.ru>
2015-03-12 14:01:30 +0300

    creation of a model question and answer model. written tests, wrote the code in the models for the test.

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Question, :type => :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'validates presence of title' do 
+    expect(Question.new(body:'question_body')).to_not be_valid
+  end
+
+  it 'validates presence of body' do 
+    expect(Question.new(title:'question_name')).to_not be_valid
+  end
+end
