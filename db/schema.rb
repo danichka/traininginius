6101e493f362210164b6d7d608455d836ad4136b
Danik Gorshkov <rock_dmj@mail.ru>
2015-03-17 13:00:59 +0300

    adding tests of association. cleaning db

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,6 @@ ActiveRecord::Schema.define(version: 20150316151313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "answer", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "id_question"
-  end
-
   create_table "answers", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
